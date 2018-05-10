e672d73a14191c94949029c7ed2a0dc5dac8d4a1
thomashancock <thomas.hancock@physics.ox.ac.uk>
2018-05-10 10:20:49 +0200

    Added WordBundle Implementation

--- Abstractions/inc/WordBundle.hpp
+++ Abstractions/inc/WordBundle.hpp
@@ -1,9 +1,40 @@
 #ifndef WORDBUNDLE_H
 #define WORDBUNDLE_H
 
+// STD
+#include <queue>
+
 class WordBundle {
 public:
-	WordBundle() { }
+	WordBundle();
+
+	//! Check if Bundle is complete
+	bool isComplete() const { return m_rocValueStored; }
+
+	//! Is bundle empty
+	bool empty() const { return m_words.empty(); }
+
+	//! Adds a word to the bundle
+	void addWord(
+		const unsigned int word
+	);
+
+	//! Returns the next word in the bundle
+	unsigned int getNextWord();
+
+	//! Returns the stored ROC value
+	unsigned int getROCValue() const;
+
+	//! Sets the ROC Value
+	void setRocValue(
+		const unsigned int rocValue
+	);
+
+private:
+	bool m_rocValueStored = false;
+
+	std::queue<unsigned int> m_words;
+	unsigned int m_rocValue = 0;
 };
 
 #endif /* WORDBUNDLE_H */
