17e336f45291fc6c4a2a7553f72c575dbaa1f64e
thomashancock <thomas.hancock@physics.ox.ac.uk>
2018-05-10 10:53:16 +0200

    Added implementation of ThreadSafeQueue

--- DataStructures/inc/ThreadSafeQueue.hpp
+++ DataStructures/inc/ThreadSafeQueue.hpp
@@ -1,9 +1,32 @@
 #ifndef THREADSAFEQUEUE_H
 #define THREADSAFEQUEUE_H
 
+// STD
+#include <queue>
+#include <mutex>
+#include <memory>
+
+template <class T>
 class ThreadSafeQueue {
 public:
-	ThreadSafeQueue() { }
+	//! Constructor
+	ThreadSafeQueue();
+
+	//! Is queue empty
+	bool empty() const;
+
+	//! Add a value to the queue
+	void push(
+		T value
+	);
+
+	//! Add a value to the queue
+	T popFront();
+
+private:
+	mutable std::mutex m_mut; //!< Mutex used for thread safety
+
+	std::queue<T> m_queue; //!< Base queue object
 };
 
 #endif /* THREADSAFEQUEUE_H */
--- DataStructures/src/ThreadSafeQueue.cpp
+++ DataStructures/src/ThreadSafeQueue.cpp
@@ -8,7 +8,54 @@
 // Public:
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
+template <class T>
+ThreadSafeQueue<T>::ThreadSafeQueue() {
+	ASSERT(m_queue.empty());
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+template <class T>
+inline bool ThreadSafeQueue<T>::empty() const {
+	// Lock Mutex
+	std::lock_guard<std::mutex> lk(m_mut);
 
+	// Return queue empty flag
+	return m_queue.empty();
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+template <class T>
+inline void ThreadSafeQueue<T>::push(
+	T value
+) {
+	// Lock mutex
+	std::lock_guard<std::mutex> lk(m_mut);
+
+	// Add value to queue
+	m_queue.push(value);
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+template <class T>
+inline T ThreadSafeQueue<T>::popFront() {
+	// Lock mutex
+	std::lock_guard<std::mutex> lk(m_mut);
+
+	// Ensure queue is not empty
+	ASSERT(!m_queue.empty());
+
+	// Get value at front of queue
+	const auto value = m_queue.front();
+
+	// Pop value from queue
+	m_queue.pop();
+
+	// Return value
+	return value;
+}
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
