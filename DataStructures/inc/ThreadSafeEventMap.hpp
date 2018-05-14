847cf29f5cefccab63c99cac9dfe5ffc60a8ca94
thomashancock <thomas.hancock@physics.ox.ac.uk>
2018-05-14 14:30:18 +0200

    Added code for ThreadSafeEventMap

--- DataStructures/inc/ThreadSafeEventMap.hpp
+++ DataStructures/inc/ThreadSafeEventMap.hpp
@@ -5,6 +5,9 @@
 #include <mutex>
 #include <memory>
 #include <map>
+#include <list>
+#include <vector>
+#include <atomic>
 
 // LOCAL
 #include "Event.hpp"
@@ -12,7 +15,9 @@
 class ThreadSafeEventMap {
 public:
 	//! Constructor
-	ThreadSafeEventMap();
+	ThreadSafeEventMap(
+		const std::list<unsigned int> tdcIDs
+	);
 
 	// Delete copy and move constructors and assignment operators
 	ThreadSafeEventMap(const ThreadSafeEventMap& other) = delete;
@@ -20,10 +25,41 @@ public:
 	ThreadSafeEventMap(const ThreadSafeEventMap&& other) = delete;
 	ThreadSafeEventMap& operator=(ThreadSafeEventMap&& other) = delete;
 
+	//! Add a packet to the events
+	void addPacket(
+		std::unique_ptr<Packet> packet
+	);
+
+	//! Is a complete event stored?
+	inline bool isCompleteStored() const;
+
+	//! Returns events up to the next complete event
+	std::vector< std::unique_ptr<Event> > popToComplete();
+
 private:
+	const std::list<unsigned int> m_tdcIDs;
+
 	mutable std::mutex m_mut; //!< Mutex used for thread safety
 
 	std::map< unsigned int, std::unique_ptr<Event> > m_map;
 };
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// Inlines:
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+bool ThreadSafeEventMap::isCompleteStored() const {
+	// Lock Mutex
+	std::lock_guard<std::mutex> lk(m_mut);
+
+	// Search for complete event in map
+	for (const auto& entry : m_map) {
+		if (entry.second->isComplete()) {
+			return true;
+		}
+	}
+	return false;
+}
 
 #endif /* THREADSAFEEVENTMAP_H */
--- DataStructures/src/ThreadSafeEventMap.cpp
+++ DataStructures/src/ThreadSafeEventMap.cpp
@@ -1,13 +1,76 @@
 #include "ThreadSafeEventMap.hpp"
 
+// STD
+#include <utility>
+
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 // Public:
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-ThreadSafeEventMap::ThreadSafeEventMap() {
+ThreadSafeEventMap::ThreadSafeEventMap(
+	const std::list<unsigned int> tdcIDs
+) :
+	m_tdcIDs(tdcIDs)
+{
 	STD_LOG("ThreadSafeEventMap Constructor Called");
 }
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void ThreadSafeEventMap::addPacket(
+	std::unique_ptr<Packet> packet
+) {
+	// Lock Mutex
+	std::lock_guard<std::mutex> lk(m_mut);
+
+	// Check for event ID in map
+	const auto eventID = packet->getEventID();
+	auto found = m_map.find(eventID);
+
+	if (found == m_map.end()) {
+		// If not found, create new event
+		m_map.insert(std::make_pair(eventID, std::make_unique<Event>(m_tdcIDs)));
+
+		// Add packet to newly created event
+		try {
+			m_map.at(eventID)->addPacket(std::move(packet));
+		} catch (std::exception& e) {
+			STD_ERR("Exception: " << e.what());
+		}
+	} else {
+		// If event with correct ID is found, add packet
+		found->second->addPacket(std::move(packet));
+	}
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+std::vector< std::unique_ptr<Event> > ThreadSafeEventMap::popToComplete() {
+	// Lock Mutex
+	std::lock_guard<std::mutex> lk(m_mut);
+
+	// Create vector to store events
+	std::vector< std::unique_ptr<Event> > returnVec;
+	ASSERT(returnVec.empty());
+
+	// Check a complete event it stored in the list
+	// If no complete event is stored, returnVec will be returned empty
+	if (isCompleteStored()) {
+		for (auto it = m_map.begin(); it != m_map.end(); /* no increment */) {
+			returnVec.push_back(std::move(it->second));
+			ASSERT(nullptr == it->second);
+			m_map.erase(it++);
+			if (returnVec.back()->isComplete()) {
+				break;
+			}
+		}
+	}
+
+	// returnVec will be moved due to RVO
+	return returnVec;
+}
+
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
