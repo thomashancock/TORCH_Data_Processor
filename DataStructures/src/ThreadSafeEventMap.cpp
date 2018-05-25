33c8f55b1c2db3c5e4853cb8f54b2f95f40670b3
thomashancock <thomas.hancock@physics.ox.ac.uk>
2018-05-25 11:41:45 +0200

    Revert "Attempted EventID/BunchID combo for event map - DOES NOT WORK"
    This reverts commit 715aa038f90368177f8e0d1ae4830f1e34875a1f.

--- DataStructures/src/ThreadSafeEventMap.cpp
+++ DataStructures/src/ThreadSafeEventMap.cpp
@@ -3,23 +3,6 @@
 // STD
 #include <utility>
 
-using key = std::pair<unsigned int, unsigned int>;
-
-// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-// Static:
-// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-const std::function<bool(const key&, const key&)> ThreadSafeEventMap::s_comparator = [] (
-	const key& k1,
-	const key& k2
-) -> bool {
-	// Sort by Bunch ID if EventIDs match, otherwise by event ID
-	// key.first = event ID
-	// key.first = bunch ID
-	return (k1.first == k2.first) ? k1.second < k2.second : k1.first < k2.first;
-};
-
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 // Public:
@@ -28,8 +11,7 @@ const std::function<bool(const key&, const key&)> ThreadSafeEventMap::s_comparat
 ThreadSafeEventMap::ThreadSafeEventMap(
 	const std::list<unsigned int> tdcIDs
 ) :
-	m_tdcIDs(tdcIDs),
-	m_map(s_comparator)
+	m_tdcIDs(tdcIDs)
 {
 	STD_LOG("ThreadSafeEventMap Constructor Called");
 }
@@ -42,30 +24,23 @@ void ThreadSafeEventMap::addPacket(
 	// Lock Mutex
 	std::lock_guard<std::mutex> lk(m_mut);
 
-	packet->print();
-
 	// Check for event ID in map
-	const auto packetKey = std::make_pair(packet->getEventID(),packet->getBunchID());
-	// const auto eventID = packet->getEventID();
-	auto found = m_map.find(packetKey);
+	const auto eventID = packet->getEventID();
+	auto found = m_map.find(eventID);
 
 	if (found == m_map.end()) {
 		// If not found, create new event
-		m_map.insert(std::make_pair(packetKey, std::make_unique<Event>(m_tdcIDs)));
+		m_map.insert(std::make_pair(eventID, std::make_unique<Event>(m_tdcIDs)));
 
 		// Add packet to newly created event
 		try {
-			m_map.at(packetKey)->addPacket(std::move(packet));
+			m_map.at(eventID)->addPacket(std::move(packet));
 		} catch (std::exception& e) {
 			STD_ERR("Exception: " << e.what());
 		}
 	} else {
 		// If event with correct ID is found, add packet
 		found->second->addPacket(std::move(packet));
-	}
-
-	if (m_map.size() > 10) {
-		WARNING("Map Bloat at " << m_map.size());
 	}
 }
 // -----------------------------------------------------------------------------
