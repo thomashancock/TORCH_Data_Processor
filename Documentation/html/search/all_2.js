4b4510b1e81f684ef8a1a48cdd4a057035ce8e5f
thomashancock <thomas.hancock@physics.ox.ac.uk>
2018-06-01 14:20:36 +0200

    Updated documentation

--- Documentation/html/search/all_2.js
+++ Documentation/html/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['channelhisto',['ChannelHisto',['../_channel_histo_8cxx.html#a850d5cc339a027442cc2f7da7fa0468c',1,'ChannelHisto.cxx']]],
+  ['channelhisto',['ChannelHisto',['../_channel_histo_8cxx.html#aad312e222f2136e4af54dc1d785e5b5c',1,'ChannelHisto.cxx']]],
   ['channelhisto_2ecxx',['ChannelHisto.cxx',['../_channel_histo_8cxx.html',1,'']]],
   ['channelmapping',['ChannelMapping',['../namespacechlmap.html#aeed247fbd4770834005177ddb2712668',1,'chlmap']]],
   ['channelmappings_2ecpp',['ChannelMappings.cpp',['../_channel_mappings_8cpp.html',1,'']]],
--- Documentation/html/search/functions_1.js
+++ Documentation/html/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['channelhisto',['ChannelHisto',['../_channel_histo_8cxx.html#a850d5cc339a027442cc2f7da7fa0468c',1,'ChannelHisto.cxx']]],
+  ['channelhisto',['ChannelHisto',['../_channel_histo_8cxx.html#aad312e222f2136e4af54dc1d785e5b5c',1,'ChannelHisto.cxx']]],
   ['config',['Config',['../class_config.html#af8b0e9dc72b16b73ebb32d177fb93e1b',1,'Config']]]
 ];
