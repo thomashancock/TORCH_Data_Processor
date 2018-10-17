eeafda6cc512163bf35e0694b286c4853af0e3ec
thomashancock <thomas.hancock@physics.ox.ac.uk>
2018-10-17 17:34:41 +0100

    Updated comments on ErrorCounter

--- Core/inc/ErrorCounter.hpp
+++ Core/inc/ErrorCounter.hpp
@@ -9,20 +9,34 @@
 // LOCAL
 #include "Debug.hpp"
 
-//! Used by ErrorSpy to track which readout board/tdc combinations errors have occured on
+//! Used by ErrorSpy to track which readout combinations of board/tdc ids that errors have occured on
+/*!
+		Template class.
+
+		Template parameter N is the dimensionality of the ErrorCounts to be stored
+		i.e. For errors specific to a board/tdc id combination, N = 2. For generic errors, N = 0.
+
+		N is given as a parameter to facilitate easier modifications to the ErrorSpy in the future.
+ */
 template <unsigned int N>
 class ErrorCounter {
 public:
 	//! Constructor
+	/*!
+		Type checking will ensure Args is a set of std::strings.
+	 */
 	template <typename... Args>
 	ErrorCounter(
-		Args... args
+		Args... args //!< Parameter pack for headings of each dimension (usually "BoardID" or "TDC")
 	);
 
-	//! Incrememnts the count for the readout board/tdc combination
+	//! Incrememnts the count for the specific combination of arguments given
+	/*!
+		Type checking will ensure Args is a set of unsigned ints.
+	 */
 	template <typename... Args>
 	inline void addCount(
-		Args... args
+		Args... args //!< Parameter pack for board/tdc ID combination
 	);
 
 	//! Prints the counts stored
@@ -31,11 +45,13 @@ public:
 private:
 	//! Returns a map key for the given readout board/tdc combination
 	/*!
-		Returns auto to facilitate easy modifiaction of map key type
+		Type checking will ensure Args is a set of unsigned ints.
+
+		Returns auto to facilitate easy modifiaction of map key type.
 	 */
 	template <typename... Args>
-	inline std::array<unsigned int, N> makeKey(
-		Args... args
+	inline auto makeKey(
+		Args... args //!< Parameter pack for board/tdc ID combination
 	) const;
 
 private:
@@ -43,25 +59,26 @@ private:
 	std::map< std::array<unsigned int, N> , unsigned int > m_counts; //!< Map to store counts in
 };
 
-// Class specialisation for N = 0
+//! Class specialisation of ErrorCounter for N = 0
 template <>
 class ErrorCounter<0> {
 public:
 	//! Constructor
+	/*!
+		No longer requires arguments as there aren't any dimensions to label.
+	 */
 	ErrorCounter() { };
 
-	//! Incrememnts the count for the readout board/tdc combination
+	//! Incrememnts the count
 	inline void addCount() { m_count++; };
 
-	//! Prints the counts stored
+	//! Prints the stored count
 	void print() const {
 		std::cout << "\t (x " << m_count << ")" << std::endl;
 	};
 
 private:
-
-private:
-	unsigned int m_count = 0;
+	unsigned int m_count = 0; //!< Stores the count
 };
 
 
@@ -69,7 +86,7 @@ private:
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-// Inlines:
+// Template and Inline functions:
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 template <unsigned int N>
@@ -86,7 +103,7 @@ ErrorCounter<N>::ErrorCounter(
 // -----------------------------------------------------------------------------
 template <unsigned int N>
 template <typename... Args>
-inline std::array<unsigned int, N> ErrorCounter<N>::makeKey(
+inline auto ErrorCounter<N>::makeKey(
 	Args... args
 ) const {
 	std::array<unsigned int, N> tmp {{ std::forward<Args>(args)... }};
