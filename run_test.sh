b3d861af8f12021844d3919703330ef911aae74a
thomashancock <thomas.hancock@physics.ox.ac.uk>
2018-10-24 13:27:51 +0100

    Changed test dataset

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-./Install/bin/DataProcessor -con Example_Config.xml -out Test_Output.root ./data/Test_Data/TimeRef_01_Device_?/*.txt
+./Install/bin/DataProcessor -con Example_Config.xml -out Test_Output.root ./data/Test_Data/MultiMCPDAQ_test_0_Chain_?_Device_?/*.dat
